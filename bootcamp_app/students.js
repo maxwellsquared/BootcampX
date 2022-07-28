f4d709fbcd8e3285e6f9e4b76f4eb6634d21d342
max k <maxkuhn@gmail.com>
2022-07-28 01:09:05 -0700

    Sanitize database inputs to avoid SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,17 +9,21 @@ const pool = new Pool({
 
 const cohortName = process.argv[2];
 const maxResults = process.argv[3];
+const values = [ cohortName, `${maxResults || 5}` ];
 const myQuery = `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${maxResults || 5};
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
 `;
 
 
-pool.query(myQuery)
+
+
+pool.query(myQuery, values)
 .then(res => {
+  console.log('\n ✨ 𝐶𝑂𝑁𝑁𝐸𝐶𝑇𝐸𝐷 ✨');
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,3 @@
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -9,24 +8,26 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
+
+const values = [ cohortName ];
 const myQuery = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohortName}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
 `;
 
-
-pool.query(myQuery)
+pool.query(myQuery, values)
 .then(res => {
   console.log('\n ✨ 𝐶𝑂𝑁𝑁𝐸𝐶𝑇𝐸𝐷 ✨')
+  console.log(res.rows);
   res.rows.forEach(user => {
     console.log(user.cohort + ": " + user.teacher);
     // console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
+  });
 });
 
 
